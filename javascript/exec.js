337a4d47d54f06cf4389f1aa8fb271a8c73218ca
Liang <zhoul@mit.edu>
2015-11-26 01:02:21 -0500

    removed sally

--- javascript/exec.js
+++ javascript/exec.js
@@ -6,63 +6,56 @@ $(document).ready(function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg36').hover(function(){
+	$('.deg40').hover(function(){
     	$("#center0").text("Weilian Chu '17");
     	$("#center1").text("Director of Internal Relations")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg72').hover(function(){
+	$('.deg80').hover(function(){
     	$("#center0").text("Liang Zhou '18");
     	$("#center1").text("Director of Publicity")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg108').hover(function(){
+	$('.deg120').hover(function(){
     	$("#center0").text("Tracy Cheng '17");
     	$("#center1").text("Director of Sales and Media")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg144').hover(function(){
+	$('.deg160').hover(function(){
     	$("#center0").text("Lucia Liu '18");
     	$("#center1").text("Director of Sales and Media")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg180').hover(function(){
+	$('.deg200').hover(function(){
     	$("#center0").text("Valentina De La Rosa '17");
     	$("#center1").text("Director of Logistics")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg216').hover(function(){
+	$('.deg240').hover(function(){
     	$("#center0").text("Monde Duinkharjav '18");
     	$("#center1").text("Director of Logistics")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg252').hover(function(){
-    	$("#center0").text("Sally Sempai '15");
-    	$("#center1").text("Shadow Captain")
-	}, function() {
-		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2015");
-	});
-	$('.deg288').hover(function(){
+	$('.deg280').hover(function(){
     	$("#center0").text("Minerva Zhou '16");
     	$("#center1").text("Director of Executive Planning")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
     	$("#center1").text("Fall 2015");
 	});
-	$('.deg324').hover(function(){
+	$('.deg320').hover(function(){
     	$("#center0").text("Abigail Choe '17");
     	$("#center1").text("Director of Finance")
 	}, function() {
