58d693832f8d23edf5470a1f7b493b8f529e7b23
Xinyi Chen <xinyic@mit.edu>
2016-12-07 12:57:39 -0500

    changed posters TAT

--- announcement.php
+++ announcement.php
@@ -5,7 +5,7 @@
 <div id='mask'></div>
 <div id='modal'>
 	<h2><b>Fairytale</b></h2>
-	<img class='modalposter' src='images/posters/reflections.png'>
+	<img class='modalposter' src='images/posters/fairytale.jpg'>
 	<p><b>Come to our Fall 2016 showcase, Fairytale (风花雪月)!</b> We'll be performing spectacular Dai and Mongolian ethnic pieces, classical and contemporary dances, as well as hip-hop pieces by Sonamoo, Seventeen, Red Velvet, and more! The showtimes are:</p>
 	<ul>
 		<li>Tuesday, December 13th, 9-11pm</li>
@@ -13,5 +13,5 @@
 	</ul>
 	<p>All shows will be held in <b>La Sala de Puerto Rico</b> on the second floor of the <a href='https://whereis.mit.edu/?go=W20'>MIT Student Center</a>. Note that this is different from our normal venue of Kresge Little Theater. Please make sure you arrive 15 minutes early or your ticket may be resold. :(</p>
 
-	<p>Tickets are $7 for MIT affiliates/$10 general admission if bought early, and $10 if bought at the door. You can see more details about sales on our Facebook event page <a href='https://www.facebook.com/events/1040802276013473/'>here!</a>
+	<p>Tickets are $7 for MIT affiliates/$10 general admission if bought early, and $10 if bought at the door. You can see more details about sales on our Facebook event page <a href='https://www.facebook.com/events/349642042071801/'>here</a>!</p>
 </div>
