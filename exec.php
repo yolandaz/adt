1d6e568c19196e9a0d300a391a7a2a5bb122ab0f
Yolanda Zhou <yolandaz@mit.edu>
2017-07-11 23:49:23 -0700

    fa17 exec

--- exec.php
+++ exec.php
@@ -5,7 +5,7 @@
 	<div class='circle-container'>
 		<h1 id='center0'>ADT Executive Board</h1>
 		<!-- Change date as appropriate -->
-		<h3 id='center1'>Spring 2016</h3>
+		<h3 id='center1'>Fall 2017</h3>
 
 		<!-- Comment/uncomment the appropriate section depending on how many people are on exec! -->
 
@@ -24,15 +24,16 @@
 
 		<!-- 10 people -->
 		
-		<img class='deg0' src='images/exec/lucy.jpg'>
-		<img class='deg40' src='images/exec/liang.jpg'>
-		<img class='deg80' src='images/exec/michelle.jpg'>
-		<img class='deg120' src='images/exec/nancyluong.jpg'>
-		<img class='deg160' src='images/exec/rachelwu.png'>
-		<img class='deg200' src='images/exec/james.jpg'>
-		<img class='deg240' src='images/exec/weilian.jpg'>
-		<img class='deg280' src='images/exec/anita.jpg'>
-		<img class='deg320' src='images/exec/vivianzhong.jpg'>
+		<img class='deg0' src='images/exec/sarahwu.jpg'>
+		<img class='deg36' src='images/exec/kedi.jpg'>
+		<img class='deg72' src='images/exec/michelle.jpg'>
+		<img class='deg108' src='images/exec/allencheng.jpg'>
+		<img class='deg144' src='images/exec/kelvin.jpg'>
+		<img class='deg180' src='images/exec/yida.png'>
+		<img class='deg216' src='images/exec/florence.png'>
+		<img class='deg252' src='images/exec/lucy.jpg'>
+		<img class='deg288' src='images/exec/james.jpg'>
+		<img class='deg324' src='images/exec/yolanda.jpg'>
 		
 	</div>
 </div>
--- javascript/exec.js
+++ javascript/exec.js
@@ -74,69 +74,75 @@ $(document).ready(function() {
 
 //10 people on exec
 */
-
 $(document).ready(function() {
 	$('.deg0').hover(function(){
-    	$("#center0").text("Xinyi (Lucy) Chen '19");
-    	$("#center1").text("Technical Director");
+    	$("#center0").text("Sarah Wu '20");
+    	$("#center1").text("Director of Marketing");
 	}, function() {
 		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
+    	$("#center1").text("Fall 2017");
 	});
-	$('.deg40').hover(function(){
-    	$("#center0").text("Liang Zhou '18");
-    	$("#center1").text("Director of Internal Relations")
+	$('.deg36').hover(function(){
+    	$("#center0").text("Kedi Hu '20");
+    	$("#center1").text("Director of Marketing")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
+    	$("#center1").text("Fall 2017");
 	});
-	$('.deg80').hover(function(){
+	$('.deg72').hover(function(){
     	$("#center0").text("Michelle Huang '19");
     	$("#center1").text("Director of Public Relations")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
+    	$("#center1").text("Fall 2017");
 	});
-	$('.deg120').hover(function(){
-    	$("#center0").text("Nancy Luong '19");
-    	$("#center1").text("Director of Marketing")
+	$('.deg108').hover(function(){
+    	$("#center0").text("Allen Cheng '20");
+    	$("#center1").text("Director of Internal Relations")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
+    	$("#center1").text("Fall 2017");
 	});
-	$('.deg160').hover(function(){
-    	$("#center0").text("Rachel Wu '19");
-    	$("#center1").text("Director of Marketing")
-	}, function() {
-		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
-	});
-	$('.deg200').hover(function(){
-    	$("#center0").text("James Roggeveen '18");
-    	$("#center1").text("Technical Director")
-	}, function() {
-		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
-	});
-	$('.deg240').hover(function(){
-    	$("#center0").text("Weilian Chu '17");
-    	$("#center1").text("Director of Executive Planning")
-	}, function() {
-		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
-	});
-	$('.deg280').hover(function(){
-    	$("#center0").text("Anita Liu '17");
-    	$("#center1").text("Director of Executive Planning")
-	}, function() {
-		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
-	});
-	$('.deg320').hover(function(){
-    	$("#center0").text("Vivian Zhong '19");
+	$('.deg144').hover(function(){
+    	$("#center0").text("Kelvin Lu '19");
     	$("#center1").text("Director of Finance")
 	}, function() {
 		$("#center0").text("ADT Executive Board");
-    	$("#center1").text("Fall 2016");
+    	$("#center1").text("Fall 2017");
+	});
+	$('.deg180').hover(function(){
+    	$("#center0").text("Yida Wang '19");
+    	$("#center1").text("Technical Director")
+	}, function() {
+		$("#center0").text("ADT Executive Board");
+    	$("#center1").text("Fall 2017");
+	});
+	$('.deg216').hover(function(){
+    	$("#center0").text("Florence Lo '19");
+    	$("#center1").text("Techincal Director")
+	}, function() {
+		$("#center0").text("ADT Executive Board");
+    	$("#center1").text("Fall 2017");
+	});
+	$('.deg252').hover(function(){
+    	$("#center0").text("Xinyi Lucy Chen '19");
+    	$("#center1").text("Director of Executive Planning")
+	}, function() {
+		$("#center0").text("ADT Executive Board");
+    	$("#center1").text("Fall 2017");
+	});
+	$('.deg288').hover(function(){
+    	$("#center0").text("James Roggeveen '18");
+    	$("#center1").text("Director of Executive Planning")
+	}, function() {
+		$("#center0").text("ADT Executive Board");
+    	$("#center1").text("Fall 2017");
+	});
+	$('.deg324').hover(function(){
+    	$("#center0").text("Yolanda Zhou '19");
+    	$("#center1").text("Cover Coordinator")
+	}, function() {
+		$("#center0").text("ADT Executive Board");
+    	$("#center1").text("Fall 2017");
 	});
 })
